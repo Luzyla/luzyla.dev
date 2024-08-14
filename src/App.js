0113cdfa47b373a3308621fd0eb7b1a41c6b411e
Luzyla <luzyla@gmail.com>
2024-08-14 05:55:02 +0200

    Add MetaData. Create MetaData component, and metadata.json. Add MetaData to App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { Skills } from "./components/Skills";
 import { Quote } from "./components/Quote";
 import { Proyectos } from "./components/Proyectos";
 import { Footer } from "./components/Footer";
+import Meta from "./components/MetaData";
 
 function App() {
   return (
     <div className="App">
+      <Meta></Meta>
       <Header></Header>
       <Hola></Hola>
       <Proyectos></Proyectos>

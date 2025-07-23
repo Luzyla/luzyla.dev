cf47166b1ac2342a8b168e7de85be807138f3c71
Luzyla <luzyla@gmail.com>
2025-07-23 18:27:28 +0200

    Apply theme to the scrollbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "./components/Header";
 import { Hola } from "./components/Hola";
 import { Proyectos } from "./components/Proyectos";
@@ -8,16 +8,16 @@ import { Skills } from "./components/Skills";
 import { Contacto } from "./components/Contacto";
 import { Footer } from "./components/Footer";
 
-
 function App() {
-const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
 
   return (
     <div className={`App ${theme}`}>
-      <Header
-        theme={theme}
-        setTheme={setTheme}
-      ></Header>
+      <Header theme={theme} setTheme={setTheme}></Header>
       <Hola></Hola>
       <Proyectos></Proyectos>
       <Quote></Quote>
